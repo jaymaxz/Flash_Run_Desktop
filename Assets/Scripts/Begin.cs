1a0ffd146dba38f2e32a2214b353adfb967e97a6
jaymaxz <jagathmax2@gamil.com>
2018-05-09 08:58:04 +0530

    Initial commit

---
+++ Assets/Scripts/Begin.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Begin : MonoBehaviour
+{
+
+    public MovieTexture movie;
+    //private AudioSource audio;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        //audio = GetComponent<AudioSource>();
+        //audio.clip = movie.audioClip;
+        movie.Play();
+        //audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!movie.isPlaying)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
